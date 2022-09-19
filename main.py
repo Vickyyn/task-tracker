a9345c8a2488df64a608e90f7d6012586d55a6e2
Vickyyn <13328@coderacademy.edu.au>
2022-09-19 18:11:40 +1000

    Exceptions added to Add feature

--- main.py
+++ main.py
@@ -4,30 +4,35 @@
 from tasks import *
 
 # Welcome
-response = input("Welcome to Task Tracker! \nToday's date is "
+main_response = input("Welcome to Task Tracker! \nToday's date is "
 f"{datetime.datetime.now().date()} \n\nPlease select from options below: \n "
 "1. View tasks \n 2. Add a new task \n 3. Edit or delete a task \n "
 "4. Complete a task \n\n")
 
-if response.lower() in {"1", "v", "view"}:
+if main_response.lower() in {"1", "v", "view"}:
     # View tasks
     # Sort tasks by variables
     print(response.lower())
 
-elif response.lower() in {"2", "a", "add"}:
+elif main_response.lower() in {"2", "a", "add"}:
+    # Add tasks
     name = input("You are adding a new task! \nWhat is the name of the task? ")
     duration = duration_fx("Approximately how much time does it take to do the task? Please input in HH:MM format. ")
-    year, month, day = date_fx("When does the task need to be completed by? Please input in DD/MM/YYYY format. ")
+    due_date = date_fx("When does the task need to be completed by? Please input in DD/MM/YYYY format. ")
 
-    # Create the task
-    new_task = Task(name, duration, year, month, day)
-    # print(new_task.values)
+    # Create the task 
+    new_task = Task(name, duration, due_date)
 
-# elif response.lower() in {"3", "e", "edit", "d", "delete"}:
+    # Confirm to user that task has been successfully created
+    # print(f"\nThe following task has been successfully added! \nName: {name} \nTime needed: {duration} minutes \nComplete by: {datetime.datetime(year, month, day).date()}")
+    print(f"\nThe following task has been successfully added! \nName: {new_task.values[0]} \nTime needed: {new_task.values[1]} minutes \nComplete by: {new_task.values[2]} \n")
+
+    print("Enter 'add' to add another task, 'back' to return to main menu, or 'quit' to exit.")
+# elif main_response.lower() in {"3", "e", "edit", "d", "delete"}:
 #     # Edit or delete a task
 #     pass
 
-# elif response.lower() in {"4", "c", "complete"}:
+# elif main_response.lower() in {"4", "c", "complete"}:
 #     # Complete a tasks 
 #     # Reward for completing tasks
 #     pass
--- tasks.py
+++ tasks.py
@@ -1,18 +1,23 @@
 import datetime
 
+class RangeError(Exception):
+    pass
+
+
 class Task:
-    def __init__(self, name, duration, year, month, day):
+    # def __init__(self, name, duration, year, month, day):
+    #     self.__name = name
+    #     self.__duration = duration
+    #     self.__due_date = datetime.datetime(year, month, day).date()
+
+    def __init__(self, name, duration, due_date):
         self.__name = name
         self.__duration = duration
-        self.__due_date = datetime.datetime(year, month, day).date()
+        self.__due_date = due_date
 
     @property
     def values(self):
-        return self.__name, self.__duration, self.__due_date.strftime('%d/%m/%Y')
-
-    @property
-    def date(self):
-        return self.__due_date
+        return self.__name, self.__duration, self.__due_date.strftime('%Y/%m/%d')
 
     @values.setter
     def values(self, name, duration, year, month, day):
@@ -22,14 +27,45 @@ class Task:
 
 
 def duration_fx(prompt):
-    user_input = input(prompt)
-    hour, minute = map(int, user_input.split(':'))
+    hour = None
+    minute = None
+    while not hour and not minute:
+        try:
+            user_input = input(prompt)
+            hour, minute = map(int, user_input.split(':'))
+            if minute >=60:
+                raise RangeError("Minutes must be between 0 and 59 inclusive")
+        except RangeError as err:
+            print(err)
+            minute = None
+        except ValueError:
+            print("You must enter two integers, separated by a colon")
+        
     return hour*60 + minute
 
 def date_fx(prompt):
-    user_input = input(prompt)
-    day, month, year = map(int, user_input.split('/'))
-    return year, month, day
+    day = None
+    month = None
+    year = None
+    while not day and not month and not year:
+        try:
+            user_input = input(prompt)
+            day, month, year = map(int, user_input.split('/'))
+        # Capture user input value errors
+        except ValueError:
+            print("You must enter three integers separated by /")
+            continue
+
+        # Utilising datetime package to see if date is valid
+        try:
+            due_date = datetime.datetime(year, month, day).date()
+        except ValueError as err:
+            # Capitalize the error message
+            print(str(err).capitalize())
+            day = None
+            month = None
+            year = None
+    return due_date
 
 # test = Task("laundry", 35, 2000, 10, 3)
 # print(test.date)
