8af1f6b337d90c39b488da90ff5be3446ceade89
Vickyyn <13328@coderacademy.edu.au>
2022-09-22 00:21:46 +1000

    made test file, added pytest

---
+++ test_main.py
@@ -0,0 +1,11 @@
+import pytest
+
+from tasks import date_fx, change_task
+from main import 
+
+
+# https://github.com/flynnwebdev/python-inheritance-lesson/blob/main/exceptions.py
+
+# https://github.com/flynnwebdev/python-inheritance-lesson/blob/main/test_exceptions.py
+
+# https://pypi.org/project/pytest/
