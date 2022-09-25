d399eab7d270a44eaa38826917c2e8c343a76320
Vickyyn <13328@coderacademy.edu.au>
2022-09-25 10:40:31 +1000

    removed pip check in bash code

--- src/run_task_tracker.sh
+++ src/run_task_tracker.sh
@@ -5,11 +5,12 @@ then
     echo "Error:
         This program needs Python 3 to run. Please check out https://www.python.org/downloads/ to install" >&2 
     exit 1
-elif ! [[ -x "$(command -v pip)" ]]
-then
-    echo "error:
-        This program needs pip to help install dependencies. Please check out https://packaging.python.org/en/latest/tutorials/installing-packages/ to install" >&2
-    exit 1
+# Pip should already be installed in a virtual environment so I've taken out this code section
+# elif ! [[ -x "$(command -v pip)" ]]
+# then
+#     echo "error:
+#         This program needs pip to help install dependencies. Please check out https://packaging.python.org/en/latest/tutorials/installing-packages/ to install" >&2
+#     exit 1
 else
     python3 -m venv venv
     source venv/bin/activate
